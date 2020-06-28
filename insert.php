3fa381d01223547b9304e6e900d32532abdb657d
jaikaranit <67534217+jaikaranit@users.noreply.github.com>
2020-06-28 12:39:53 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,8 +2,8 @@
 <body>
 
 <?php
-$out=$_GET['txt'];
-$myJSON = json_decode($out);
+$out=$_GET['txt'];  //get parameter using GET method
+$myJSON = json_decode($out);  //decoding array to json 
 $dept=$myJSON->dept;
 $email=$myJSON->email;
 $phone=$myJSON->phone;
@@ -12,16 +12,16 @@ $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
  $db = "kec";
- $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
-$sql="insert into events(department,email,phone,college)values('$dept','$email',$phone,'$college')";
-if ($conn->query($sql) === TRUE) {
+ $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$db);  //connecting to database(mysql)
+$sql="insert into events(department,email,phone,college)values('$dept','$email',$phone,'$college')";  //insert a record using querie(mysqli)
+if ($conn->query($sql) === TRUE) {  //execution of querie using condition
   $arr = array('value' => "yes");
-		echo json_encode($arr);
-		die;
+		echo json_encode($arr);  //returns yes
+		die;  //end of the file
 } else {
   $arr = array('value' => "no");
-		echo json_encode($arr);
-		die;
+		echo json_encode($arr);  //returns no
+		die;  //end of the file
 }
 ?>
 
