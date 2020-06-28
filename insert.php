b32b99a5e4e963da1a332bc7c9e34f7b6a09dbe6
jaikaranit <67534217+jaikaranit@users.noreply.github.com>
2020-06-28 13:01:00 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -13,6 +13,14 @@ $dbhost = "localhost";
  $dbpass = "";
  $db = "kec";
  $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$db);  //connecting to database(mysql)
+$sql="select * from events where department='$dept' and email='$email'";
+ $disp = mysqli_query($conn, $sql);
+ 
+ if ($row =mysqli_fetch_array($disp,MYSQLI_ASSOC)) {
+  $arr = array('value' => "yes");
+		echo json_encode($arr);
+		die;
+} else {	
 $sql="insert into events(department,email,phone,college)values('$dept','$email',$phone,'$college')";  //insert a record using querie(mysqli)
 if ($conn->query($sql) === TRUE) {  //execution of querie using condition
   $arr = array('value' => "yes");
@@ -22,6 +30,7 @@ if ($conn->query($sql) === TRUE) {  //execution of querie using condition
   $arr = array('value' => "no");
 		echo json_encode($arr);  //returns no
 		die;  //end of the file
+}
 }
 ?>
 
